f6275cf1f9bd5e1c3135c0651fb1d44f7f9b5d1e
Mattt Zmuda <mattt@me.com>
2025-11-09 07:33:28 -0500

    Remove unused MLXLanguageModelError enum

--- Sources/AnyLanguageModel/Models/MLXLanguageModel.swift
+++ Sources/AnyLanguageModel/Models/MLXLanguageModel.swift
@@ -163,19 +163,6 @@ import Foundation
         }
     }
 
-    /// Errors that can occur when working with MLX language models.
-    public enum MLXLanguageModelError: LocalizedError {
-        /// Image segments are not supported in MLXLanguageModel
-        case unsupportedFeature
-
-        public var errorDescription: String? {
-            switch self {
-            case .unsupportedFeature:
-                return "This MLXLanguageModel does not support image segments"
-            }
-        }
-    }
-
     // MARK: - Options Mapping
 
     private func toGenerateParameters(_ options: GenerationOptions) -> MLXLMCommon.GenerateParameters {
